0b39233afb126c65bf2cc4227ae93ccd20bd2bcb
Jakub Tutka <jakubtutka02@gmail.com>
2024-03-21 10:44:32 +0100

    done

--- iteracyjny_serwer/server.py
+++ iteracyjny_serwer/server.py
@@ -5,13 +5,15 @@ PORT = 7
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
     sock.bind((IP, PORT))
+    client_id = 0
 
     while True:
         sock.listen(1)
         connection, address = sock.accept()
+        client_id += 1
 
         guest_ip = address[0]
-        print(f"Connection from {guest_ip}")
+        print(f"Connection from #{client_id} {guest_ip}")
 
         while True:
             try:
--- wielowatkowy_serwer/server.py
+++ wielowatkowy_serwer/server.py
@@ -5,8 +5,8 @@ IP = "0.0.0.0"
 PORT = 7
 
 
-def handle_client_connection(connection, ip):
-    print(f"Connected with {ip}")
+def handle_client_connection(connection, ip, client_id):
+    print(f"Connected with #{client_id} {ip}")
 
     while True:
         try:
@@ -23,13 +23,15 @@ def handle_client_connection(connection, ip):
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
     sock.bind((IP, PORT))
+    client_id = 0
 
     while True:
         sock.listen(1)
         connection, address = sock.accept()
+        client_id += 1
 
         guest_ip = address[0]
 
         threading.Thread(
-            target=handle_client_connection, args=(connection, guest_ip)
+            target=handle_client_connection, args=(connection, guest_ip, client_id)
         ).start()
